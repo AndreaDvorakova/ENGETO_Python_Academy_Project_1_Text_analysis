77a051f3a73d9792fc83cd35c7e5fa9629b69e5b
AndreaDvorakova <csajka.andi@gmail.com>
2022-03-04 18:37:53 +0100

    vytvoreni konstantu a overeni prihlasovacich udaju

---
+++ Users.py
@@ -0,0 +1,5 @@
+USERS = ('bob', 'ann', 'mike', 'liz')
+PASSWORDS = ('123', 'pass123', 'password123', 'pass123')
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from Users import *
+from Constants import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    username_in = input('Please enter your username: ')
+    password_in = input('Please enter your password: ')
+
+    if username_in in USERS and (PASSWORDS[USERS.index(username_in)] == password_in):
+        print('nabidka')
+    else:
+        print('nic')
+
+
