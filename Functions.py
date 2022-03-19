edac65b09bb265ce3d08d66c345d19d639c197d9
AndreaDvorakova <csajka.andi@gmail.com>
2022-03-19 15:01:06 +0100

    vyber textu podle uzivatele

--- Functions.py
+++ Functions.py
@@ -5,35 +5,38 @@ slova_title = []
 slova_upper = []
 slova_lower = []
 slova_cisla = []
-suma_celkem = 0
 slovnik = {}
 i = 1
+cislo_in = ''
+volba = ''
 
-list_slov = TEXTS[0].split()
-for slovo in list_slov:
-    ciste_slovo = slovo.strip(',.-')
-    cista_slova.append(ciste_slovo)
+def statistika_textu (cislo_in):
+    list_slov = TEXTS[cislo_in].split()
+    for slovo in list_slov:
+        ciste_slovo = slovo.strip(',.-')
+        cista_slova.append(ciste_slovo)
 
-for slovo in cista_slova:
-    if slovo.istitle():
-        slova_title.append(slovo)
-    elif slovo.isalpha() and slovo.isupper():
-        slova_upper.append(slovo)
-    elif slovo.islower():
-        slova_lower.append(slovo)
-    elif slovo.isnumeric():
-        slova_cisla.append(slovo)
+    for slovo in cista_slova:
+        if slovo.istitle():
+            slova_title.append(slovo)
+        elif slovo.isalpha() and slovo.isupper():
+            slova_upper.append(slovo)
+        elif slovo.islower():
+            slova_lower.append(slovo)
+        elif slovo.isnumeric():
+            slova_cisla.append(slovo)
+    print('There are ', len(cista_slova), ' words in the selected text.')
+    print('There are ', len(slova_title), ' titlecase words.')
+    print('There are ', len(slova_upper), ' uppercase words.')
+    print('There are ', len(slova_lower), ' lowercase words.')
+    print('There are ', len(slova_cisla), ' numeric strings.')
+    soucet_celkem = 0
+    for cislo in slova_cisla:
+        soucet_celkem = soucet_celkem + int(cislo)
+    print('The sum of all the numbers ', soucet_celkem, '.')
 
-for cislo in slova_cisla:
-    suma_celkem = suma_celkem + int(cislo)
-
-pocet_slov = len(cista_slova)
-pocet_slova_title = len(slova_title)
-pocet_slova_upper = len(slova_upper)
-pocet_slova_lower = len(slova_lower)
-pocet_slova_cisla = len(slova_cisla)
-
-for slovo in cista_slova:
-    l = len(slovo)
-    slovnik[l] = slovnik.get(l, 0) + 1
+def statistika_slov_graficky ():
+    for slovo in cista_slova:
+        l = len(slovo)
+        slovnik[l] = slovnik.get(l, 0) + 1
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from Users import *
 from Constants import *
 from Functions import *
-import pprint
 
 if __name__ == '__main__':
 
@@ -12,23 +11,20 @@ if __name__ == '__main__':
         print(ODDELOVAC)
         print('Welcome to the app, ', username_in, '!\nWe have 3 texts to be analyzed.')
         print(ODDELOVAC)
-        cislo_in = (input('Enter a number btw. 1 and 3 to select:'))
-        if cislo_in.isdigit() and 1 <= int(cislo_in) < 3:
-            print('There are ', pocet_slov, ' words in the selected text.')
-            print('There are ', pocet_slova_title, ' titlecase words.')
-            print('There are ', pocet_slova_upper, ' uppercase words.')
-            print('There are ', pocet_slova_lower, ' lowercase words.')
-            print('There are ', pocet_slova_cisla, ' numeric strings.')
-            print('The sum of all the numbers ', suma_celkem, '.')
+        volba = input('Enter a number btw. 1 and 3 to select: ')
+        cislo_in = int(volba) - 1
+        if volba.isdigit() and 1 <= int(volba) <= 3:
+            statistika_textu(cislo_in)
+            statistika_slov_graficky()
             print(ODDELOVAC)
             print("{:<5}{:<20}{:<4}".format('LEN |', ' OCCURENCES ', ' | NR.'))
             print(ODDELOVAC)
             for k, v in slovnik.items():
                 pocet = '*' *  k
-                print(f'{i:3} | {pocet:19} | {k:4d}')
+                print(f'{i:3} | {pocet:19} | {k:2d}')
                 i = i + 1
         else:
-            print('The choice ', cislo_in, 'is not in the offer. Terminating the program...')
+            print('The choice', volba, 'is not in the offer. Terminating the program...')
     else:
         print('Unregistered user, terminating the program...')
 
